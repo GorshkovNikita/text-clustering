e5ea53064a701a193d80fd2a8400845b3b30f241
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2017-02-11 10:28:20 +0300

    REfactored get neighbours method

--- src/main/java/diploma/clustering/dbscan/points/DbscanStatusesCluster.java
+++ src/main/java/diploma/clustering/dbscan/points/DbscanStatusesCluster.java
@@ -26,7 +26,14 @@ public class DbscanStatusesCluster extends StatusesCluster implements DbscanPoin
         this.clusterId = clusterId1;
     }
 
-    // TODO: подумать, как это сделать эффективно, так как количество кластеров может быть очень большим
+    /**
+     * Поиск соседей с помощью косинуса угла между векторами tf-idf текущего кластера и всех остальных
+     * TODO: подумать, как это сделать эффективно, так как количество кластеров может быть очень большим
+     * @param clusters - кластера, из которых нужно найти соседние
+     * @param eps - порог, по которому определяется является ли точка соседом или нет,
+     *            в данном случае - это значение косинуса угла
+     * @return - соседи
+     */
     @Override
     public List<DbscanPoint> getNeighbours(List<? extends DbscanPoint> clusters, double eps) {
         Map<String, Integer> frequentTerms = new HashMap<>();
@@ -41,18 +48,11 @@ public class DbscanStatusesCluster extends StatusesCluster implements DbscanPoin
         List<DbscanPoint> neighbours = new ArrayList<>();
         for (DbscanPoint cluster: clusters) {
             if (cluster != this)
-                if (CosineSimilarity.cosineSimilarity(this.getTfIdf().getTfIdfForAllDocuments()
-                                .entrySet().stream()
-                                .filter(entry ->
-                                        !frequentTerms.containsKey(entry.getKey()) ||
-                                                (((double) frequentTerms.get(entry.getKey()) / (double) clusters.size() <= 0.2)))
-                                .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue())),
-                        ((DbscanStatusesCluster) cluster).getTfIdf().getTfIdfForAllDocuments()
-                                .entrySet().stream()
-                                .filter(entry ->
-                                        !frequentTerms.containsKey(entry.getKey()) ||
-                                                (((double) frequentTerms.get(entry.getKey()) / (double) clusters.size() <= 0.2)))
-                                .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()))) >= eps)
+                if (CosineSimilarity.cosineSimilarity(
+                        filterTfIdfByFrequentTerms(this.getTfIdf().getTfIdfForAllDocuments(), frequentTerms, clusters.size()),
+                        filterTfIdfByFrequentTerms(((DbscanStatusesCluster) cluster).getTfIdf().getTfIdfForAllDocuments(), frequentTerms, clusters.size())
+                )
+                        >= eps)
                     neighbours.add(cluster);
         }
         return neighbours;
@@ -79,5 +79,22 @@ public class DbscanStatusesCluster extends StatusesCluster implements DbscanPoin
         // TODO: при этом нужно сохранять id твита
         // TODO: Также, возможно, нужно удалять все из TfIdf, кроме tfIdfMapForAllDocuments
         this.clusterId = clusterId;
+    }
+
+    /**
+     * Фильтрация вектора tf-idf часто встречающимися термами.
+     * @param tfIdfVector - фильтруемый вектор tf-idf
+     * @param frequentTerms - часто встречающиеся термы
+     * @param numberOfClusters - количество кластеров
+     * @return - отфильтрованный вектор tf-idf
+     */
+    private Map<String, Double> filterTfIdfByFrequentTerms(Map<String, Double> tfIdfVector, Map<String, Integer> frequentTerms, int numberOfClusters) {
+        return tfIdfVector.entrySet().stream()
+                .filter(entry ->
+                        // пропускает термы, не входящие в карту частых термов
+                        !frequentTerms.containsKey(entry.getKey()) ||
+                                // или термы, которые встречаются реже, чем в каждом 5-ом кластере
+                                (((double) frequentTerms.get(entry.getKey()) / (double) numberOfClusters <= 0.2)))
+                .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
     }
 }
